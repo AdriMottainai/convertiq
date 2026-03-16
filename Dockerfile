a2207dd9129441c2d51ff6558bfd520f48699232
Dominique Turpin <turpin.dominique@gmail.com>
2026-03-16 18:17:01 +0100

    first API

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ COPY requirements_prod.txt .
 COPY requirements_dev.txt .
 COPY setup.py .
 COPY convertiq_py ./convertiq_py
+COPY save_models ./model_lgbm_baseline.pkl
 RUN pip install -r requirements_prod.txt && \
     pip install -e .
 CMD ["uvicorn", "convertiq_py.api.fast:app", "--host", "0.0.0.0", "--port", "8000"]
--- convertiq_py/api/fast.py
+++ convertiq_py/api/fast.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from convertiq_py.ml_logic.preprocessor import preprocess_features, feature_engineering
 from convertiq_py.ml_logic.data import load_data_kaggle_raw, load_data_in_chunks, clean_data
 from convertiq_py.ml_logic.registry import load_model
@@ -8,16 +7,6 @@ from convertiq_py.ml_logic.registry import load_model
 app = FastAPI()
 app.state.model = load_model()
 
-# Allowing all middleware is optional, but good practice for dev purposes
-'''app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)'''
-
-# http://127.0.0.1:8000/predict?pickup_datetime=2014-07-06+19:18:00&pickup_longitude=-73.950655&pickup_latitude=40.783282&dropoff_longitude=-73.984365&dropoff_latitude=40.769802&passenger_count=2
 @app.get("/predict")
 def predict(
         df,  # Format df initial
